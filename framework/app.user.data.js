a15582c8f8ed50a16532cb9efde7a28fe9f85b1a
researchranks <x@x-3.local>
2016-09-09 13:07:30 -0700

    updated forEach loop and for loop to get the data from the json tree

--- framework/app.user.data.js
+++ framework/app.user.data.js
@@ -12,35 +12,215 @@ define(function(){
       var value_key = '';
 
 
+     //  function getProperties(o) {
+     //      var results = [];
+     //      function properties(obj) {
+     //          var props, i;
+     //          if (obj === null) {
+     //              return results;
+     //          }
+     //          if (typeof obj !== 'object' && typeof obj !== 'function') {
+     //              return properties(obj.constructor.prototype);
+     //          }
+     //          props = Object.getOwnPropertyNames(obj);
+     //          i = props.length;
+     //          while (i--) {
+     //              if (!~results.indexOf(props[i])) {
+     //                  results.push(props[i]);
+     //              }
+     //          }
+     //          return properties(Object.getPrototypeOf(obj));
+     //      }
+     //      return properties(o);
+     //  }
 
-        firebase.database()
-        .ref()
-        .once('value')
+     //  function getValuesByKey(object, key) {
+     //   var values = [];
+     //   JSON.stringify(object, function(k, v) {
+     //    if (k === key) values.push(v);
+     //    return v;
+     //   });
+     //   return values;
+     // }
+
+
+     function process(key,value) {
+          if(typeof(value) == 'object'){
+               console.log( key + ' : ' + 'object');
+          }else{
+               console.log( key + ' : ' + value);
+          }
+     }
+
+     function traverse(o,func) {
+         for (var i in o) {
+             func.apply(this,[i,o[i]]);
+             if (o[i] !== null && typeof(o[i])=="object") {
+                 //going on step down in the object tree!!
+                 traverse(o[i],func);
+             }
+         }
+     }
+
+        var query = firebase.database()
+        .ref('users').orderByKey();
+
+
+        query.once('value')
         .then(
           function(snapshot){
+             var data = snapshot.val();
+             var a = '';
+
+            snapshot.forEach(function(users){
+
+                user_id = users.key;
+                profile_keys = Object.keys( users.exportVal() );
+                 console.log( user_id );
+                 user_data +=
+                 '<mark>' +
+                  user_id +
+                  '</mark>';
+
+
+                 for(var i in profile_keys ){
+
+                      var profile_key = profile_keys[i];
+                      var profile_data = profile_key;
+
+                    user_data +=
+                    '<dl>' +
+                    '<dt>' +
+                         profile_key +
+                    '</dt>' +
+                         '<dd>' +
+                              data [user_id] [ profile_data ] +
+                         '</dd>' +
+                    '</dl>'
+                    ;
+
+                     console.log(
+
+                         profile_key +
+                          ' : ' +
+                         data
+                         [user_id]
+                         [
+                              profile_data
+                         ]
+
+                     );
+                 }
+
+               //    for(var profile in data[users.key] ){
+               //        console.log( user_key );
+               //        console.log( data[users.key][profile]  );
+               //    }
+
+               //    for(var user_id in users.key){
+               //         console.log( users.key );
+               //    }
+
+            });
+
+        document.getElementById('app-user-data').innerHTML = user_data;
+
+          // console.log( snapshot.val() );
+          // console.log( snapshot.key );
+          // console.log( snapshot.child('0').child('type').key );
+          // console.log( snapshot.child('0').child('type').val() );
+
+
+          // traverse(data,process);
+
+
+
+            //  var vals = Object.keys(data).map(function (key) {.val()
+            //      return data[key];
+            //  });
+            //
+            //  console.log(vals);
+            //
+            //
+            //  var keys = Object.keys(data);
+            //
+            // for (var i = 0; i < keys.length; i++) {
+            //     var val_a = data[keys[i]];
+            //     console.log(val_a);
+            // }
+            //
+            // console.log('----------------------------------');
+            //
+
+          //    for (var key in data) {
+          //       if (Object.prototype.hasOwnProperty.call(data, key)) {
+          //           var val_b = data[key];
+          //           console.log(val_b);
+          //       }
+          //   }
+            //  a = getProperties(data);
+            //  console.log(a);
+             //
+            // Object.keys(data).forEach(
+            //       function( key ){
+            //       console.log(key);
+            //       console.log( data[key]  );
+            //
+            // });
+
+             //
+            //  results = snapshot.val();
+             //
+             //
+            //  Object.keys(results).forEach(function (prop) {
+            //    var value = results[prop];
+             //
+            //       console.log( results[prop] );
+             //
+            //       for(var i in value){
+            //             console.log(  value[i] );
+            //       }
+            //  });
+
+
+
+            //  results = JSON.parse(results);
+
+            //   Object.keys(results).forEach(function(value){
+            //             console.log(value);
+            //             Object.keys(value).forEach(function(data){
+            //                   console.log(
+            //                         results[data]
+            //                   );
+            //             });
+            //    });
+
 
              // results = JSON.stringify(snapshot.val());
             //     turn json back to object
             //     results = JSON.parse(results);
 
-            Object.keys( snapshot.val() ).forEach(function(key){
-                  console.log(key);
-            });
-
-            Object.keys( snapshot.val() ).forEach(function(key){
-
-                        for(var value in snapshot.val()[key]){
-                              console.log(value);
-
-                              console.log(
-                                    Object.keys(
-                                          snapshot.val()[key][value]
-                                    )
-                              );
-
-                        }
-
-            });
+            // Object.keys( snapshot.val() ).forEach(function(key){
+            //       console.log(key);
+            // });
+            //
+            //
+            //
+            // Object.keys( snapshot.val() ).forEach(function(key){
+            //
+            //             for(var value in snapshot.val()[key]){
+            //                   console.log(value);
+            //
+            //       //console.log( snapshot.val()[key].hasOwnProperty[value] ) ;
+            //                   // console.log(
+            //                   //       Object.keys(
+            //                   //             snapshot.val()[key][value]
+            //                   //       )
+            //                   // );
+            //
+            //             }
+            //
+            // });
 
                   // Object.keys( snapshot.val()[key])
                   // .foreach(function(value){
